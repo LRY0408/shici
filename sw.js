67eaf710e660a635b61583c962524dceba0b5ab5
LRY0408 <702685419@qq.com>
2026-09-01 16:26:15 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,43 @@
+// 拾词 Service Worker - 离线缓存
+const CACHE_NAME = 'shici-v1';
+const urlsToCache = [
+    './',
+    'index.html',
+    'app-mobile.js',
+    'vocab_data.js',
+    'manifest.json',
+    'icon-192.png',
+    'icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            if (response) return response;
+            return fetch(event.request).then((response) => {
+                if (!response || response.status !== 200 || response.type !== 'basic') return response;
+                let responseToCache = response.clone();
+                caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseToCache));
+                return response;
+            });
+        })
+    );
+});
